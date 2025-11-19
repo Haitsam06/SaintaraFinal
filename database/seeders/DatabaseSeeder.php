b47f081af32d40abca9029e1e00dec6247cca722
Gugugaga25 <banupandaraa@gmail.com>
2025-11-19 21:09:03 +0700

    penambahan seeder roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,20 +8,26 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    public function run()
+    {
+        $this->call([
+            RolesSeeder::class,
+        ]);
+    }
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+    // public function run(): void
+    // {
+    //     // User::factory(10)->create();
 
-        User::firstOrCreate(
-            ['email' => 'test@example.com'],
-            [
-                'name' => 'Test User',
-                'password' => 'password',
-                'email_verified_at' => now(),
-            ]
-        );
-    }
+    //     User::firstOrCreate(
+    //         ['email' => 'test@example.com'],
+    //         [
+    //             'name' => 'Test User',
+    //             'password' => 'password',
+    //             'email_verified_at' => now(),
+    //         ]
+    //     );
+    // }
 }
---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RolesSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('roles')->insert([
+            [
+                'id' => 1,
+                'role_user' => 'superadmin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'role_user' => 'admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 3,
+                'role_user' => 'customer',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
