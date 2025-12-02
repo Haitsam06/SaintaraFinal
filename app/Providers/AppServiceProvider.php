e2ff45942ec5ebc2a3411941cc5dff35398ad479
fitrahmoef <fitrahmoef@gmail.com>
2025-12-02 17:39:50 +0700

    baru

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,18 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // <- TAMBAHKAN BARIS INI
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        // Paksa semua URL (route, asset, dll) pakai HTTPS di non-local env
-        if (config('app.env') !== 'local') {
-            URL::forceScheme('https');
-        }
+        //
     }
 }
